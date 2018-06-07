7613a1b7aeb7f016a5b84865d11f6c4a6f70283c
zrobiao <zrobiao@163.com>
2018-06-07 16:09:45 +0800

    修改header部分、banner滑动

--- src/components/common/header.vue
+++ src/components/common/header.vue
@@ -1,21 +1,21 @@
 <template>
   <header>
-    <navbar>
+    <navbar class="container hph-navbar">
       <a class="navbar-brand logo-link" slot="brand" href="index.html">
         <img src="../../assets/logo.png" class="img-responsive">
       </a>
       <template slot="collapse">
         <navbar-nav right>
-          <li :class="[menuTitle==='首页'?'df-active':'','submenu-box']">
+          <li :class="[menuTxt==='首页'?'df-active':'','submenu-box']">
             <a href="index.html" class="submenu">首页</a>
           </li>
-          <li :class="[menuTitle==='健康资源'?'df-active':'','submenu-box']">
+          <li :class="[menuTxt==='健康资源'?'df-active':'','submenu-box']">
             <a href="healthInfo.html" class="submenu">健康资源</a>
           </li>
-          <li :class="[menuTitle==='资讯'?'df-active':'','submenu-box']">
+          <li :class="[menuTxt==='资讯'?'df-active':'','submenu-box']">
             <a href="infoList.html" class="submenu">资讯</a>
           </li>
-          <li :class="[menuTitle==='关于我们'?'df-active':'','submenu-box']">
+          <li :class="[menuTxt==='关于我们'?'df-active':'','submenu-box']">
             <a href="aboutUs.html" class="submenu">关于我们</a>
           </li>
         </navbar-nav>
@@ -26,23 +26,48 @@
 <script>
 import '../../assets/bootstrap/css/bootstrap.min.css'
 export default {
-  psops: ['menu-title'],
+  props: ['menuTitle'],
   data () {
-    return {}
+    return {
+      menuTxt:this.menuTitle,
+    }
   },
   created () {
-    console.log(this.menuTitle)
+  },
+  mounted (){
   },
   methods: {},
-  components: {}
+  components: {},
 }
 </script>
 <style>
+header{
+  border-bottom:1px solid #e5e5e5;
+}
+.hph-navbar{
+  background: #fff;
+  border: none;
+  margin-bottom: 6px;
+}
 .logo-link {
   padding: 4px 6px;
 }
 .logo-link img {
   /* padding: 4px 6px; */
   height: 100%;
+}
+.submenu-box:hover{
+	border-bottom: 2px solid #34b8de;
+}
+.submenu-box>a{
+  color:#525252!important;
+  font-size: 1.6rem;
+}
+.df-active{
+	border-bottom: 2px solid #34b8de;
+}
+.df-active>a{
+	background-color: #fff!important;
+  color:#34b8de!important;
 }
 </style>
--- src/module/index/index.vue
+++ src/module/index/index.vue
@@ -1,28 +1,76 @@
 <template>
-  <div id="app">
+  <div>
     <Header menu-title="首页"></Header>
-    <h2>{{msg}}</h2>
-    <footer></footer>
+    <section>
+      <carousel :indicators="indicators" :controls="controls" :interval="interval" ref="carousel">
+        <slide v-for="(slide, index) in slides" :key="index">
+          <div style="width: 100%;height: 400px;">
+            <img :src="slide.src" class="img-responsive">
+          </div>
+        </slide>
+        <template slot="indicators" slot-scope="props">
+          <ol class="carousel-indicators custom-carousel-indicators">
+            <li v-for="(slide, index) in slides" :class="{active:index === props.activeIndex}" @click="props.select(index)">
+              <!-- Anything you like here -->
+            </li>
+          </ol>
+        </template>
+      </carousel>
+    </section>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-import Header from '../../components/common/header.vue'
-import footer from '../../components/common/footer.vue'
+import Header from "../../components/common/header.vue";
+import Footer from "../../components/common/footer.vue";
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    footer,
+    Footer,
     Header
   },
-  data () {
+  data() {
     return {
-      msg: '这里是首页！'
-    }
-  }
-}
+      msg: "这里是首页！",
+      interval: 5000,
+      indicators: true,
+      controls: false,
+      slides: [
+        {
+          title: "滑动1",
+          src: "../../../static/img/index_banner.jpg"
+        },
+        {
+          title: "滑动2",
+          src: "../../../static/img/index_banner.jpg"
+        },
+        {
+          title: "滑动3",
+          src: "../../../static/img/index_banner.jpg"
+        }
+      ]
+    };
+  },
+  created() {}
+};
 </script>
 
 <style>
-
+/* slides样式 */
+.custom-carousel-indicators li{
+  margin: 0 8px;
+  border-radius: 50%;
+  border:none;
+  background: #fff;
+  width: 15px;
+  height: 15px;
+}
+.custom-carousel-indicators .active{
+  border-radius: 50%;
+  margin: 0 8px;
+  width: 15px;
+  height: 15px;
+  background: #34b8de;
+}
 </style>
