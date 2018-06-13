f19a6a7f9b1a2bf9080898689e179c84a2422700
zrobiao <zrobiao@163.com>
2018-06-13 17:24:14 +0800

    删除多余项

--- src/module/healthInfo/healthInfo.js
+++ src/module/healthInfo/healthInfo.js
@@ -1,11 +1,13 @@
 import Vue from 'Vue'
 import healthInfo from './healthInfo.vue'
 import * as uiv from 'uiv'
+import router from './router';
 
 Vue.use(uiv)
 Vue.config.productionTip = false
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  router,
   render: h => h(healthInfo)
 })
